7e692ab296bf3f449288d55b22298646cd7dd250
Luka <68041537+lukadsant@users.noreply.github.com>
2023-07-07 16:17:15 +0000

    frase modificada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return '<h1>Home<h1/>'
-}
 
-export default Home;
+    const navegador = window.navigator.userAgent; // Nome do navegador
+    const plataforma = window.navigator.platform; // Plataforma do sistema operacional
+    const idioma = window.navigator.language; // Idioma do navegador
+    const larguraTela = window.innerWidth; // Largura da tela em pixels
+    const alturaTela = window.innerHeight; // Altura da tela em pixels
+
+    return `<h1>Bem-vindo visitante!</h1>
+            <p>Seu navegador é: ${navegador}</p>
+            <p>Sistema operacional: ${plataforma}</p>
+            <p>Idioma: ${idioma}</p>
+            <p>Largura da tela: ${larguraTela}px</p>
+            <p>Altura da tela: ${alturaTela}px</p>`;
+  }
+  
